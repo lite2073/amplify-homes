471d26296d244df28f277a33d0083a98711b463a
Te L <lite2073+git@gmail.com>
2021-12-04 12:06:15 -0800

    follow https://aws.amazon.com/blogs/mobile/aws-amplify-studio-figma-to-fullstack-react-app-with-minimal-programming/

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {HomeListings, NavBar, MarketingFooter} from './ui-components';
 
 function App() {
   return (
     <div className="App">
+        <NavBar width={"100vw"}/>
+        <HomeListings isPaginated itemsPerPage={3}/>
+        <MarketingFooter width={"100vw"}/>
     </div>
   );
 }
